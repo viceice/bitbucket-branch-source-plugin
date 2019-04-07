viceice/bitbucket-branch-source-plugin

commit 868676d61ce3847547b6860b928d07c1679c144f

JENKINS-48737 Fixing lightweight pull request checkouts, also for files in subdirectories (#174)

src/main/java/com/cloudbees/jenkins/plugins/bitbucket/server/client/BitbucketServerAPIClient.java
@@ -147,7 +147,7 @@ public class BitbucketServerAPIClient implements BitbucketApi {
     private static final String API_PULL_REQUESTS_PATH = API_REPOSITORY_PATH + "/pull-requests{?start,limit,at,direction,state}";
     private static final String API_PULL_REQUEST_PATH = API_REPOSITORY_PATH + "/pull-requests/{id}";
     private static final String API_PULL_REQUEST_MERGE_PATH = API_REPOSITORY_PATH + "/pull-requests/{id}/merge";
-    private static final String API_BROWSE_PATH = API_REPOSITORY_PATH + "/browse{/path*}{?at}";
+    static final String API_BROWSE_PATH = API_REPOSITORY_PATH + "/browse/{+path}{?at}";
     private static final String API_COMMITS_PATH = API_REPOSITORY_PATH + "/commits{/hash}";
     private static final String API_PROJECT_PATH = API_BASE_PATH + "/projects/{owner}";
     private static final String API_COMMIT_COMMENT_PATH = API_REPOSITORY_PATH + "/commits{/hash}/comments";

src/test/java/com/cloudbees/jenkins/plugins/bitbucket/server/client/BitbucketServerAPIClientTest.java
@@ -1,0 +1,34 @@
+package com.cloudbees.jenkins.plugins.bitbucket.server.client;
+
+import com.damnhandy.uri.template.UriTemplate;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static com.cloudbees.jenkins.plugins.bitbucket.server.client.BitbucketServerAPIClient.API_BROWSE_PATH;
+
+public class BitbucketServerAPIClientTest {
+
+    @Test
+    public void repoBrowsePathFolder() {
+        String expand = UriTemplate
+            .fromTemplate(API_BROWSE_PATH)
+            .set("owner", "test")
+            .set("repo", "test")
+            .set("path", "folder/Jenkinsfile")
+            .set("at", "fix/test")
+            .expand();
+        Assert.assertEquals("/rest/api/1.0/projects/test/repos/test/browse/folder/Jenkinsfile?at=fix%2Ftest", expand);
+    }
+
+    @Test
+    public void repoBrowsePathFile() {
+        String expand = UriTemplate
+            .fromTemplate(API_BROWSE_PATH)
+            .set("owner", "test")
+            .set("repo", "test")
+            .set("path", "Jenkinsfile")
+            .expand();
+        Assert.assertEquals("/rest/api/1.0/projects/test/repos/test/browse/Jenkinsfile", expand);
+    }
+
+}

src/main/java/com/cloudbees/jenkins/plugins/bitbucket/filesystem/BitbucketSCMFileSystem.java
@@ -32,6 +32,7 @@ import com.cloudbees.jenkins.plugins.bitbucket.PullRequestSCMHead;
 import com.cloudbees.jenkins.plugins.bitbucket.api.BitbucketApi;
 import com.cloudbees.jenkins.plugins.bitbucket.api.BitbucketApiFactory;
 import com.cloudbees.jenkins.plugins.bitbucket.api.BitbucketAuthenticator;
+import com.cloudbees.jenkins.plugins.bitbucket.client.BitbucketCloudApiClient;
 import com.cloudbees.plugins.credentials.CredentialsMatchers;
 import com.cloudbees.plugins.credentials.CredentialsProvider;
 import com.cloudbees.plugins.credentials.common.StandardCredentials;
@@ -139,17 +140,36 @@ public class BitbucketSCMFileSystem extends SCMFileSystem {
             BitbucketAuthenticator authenticator = AuthenticationTokens.convert(BitbucketAuthenticator.authenticationContext(serverUrl), credentials);
 
             BitbucketApi apiClient = BitbucketApiFactory.newInstance(serverUrl, authenticator, owner, repository);
-            String ref;
+            String ref = null;
+
             if (head instanceof BranchSCMHead) {
                 ref = head.getName();
             } else if (head instanceof PullRequestSCMHead) {
+                // working on a pull request - can be either "HEAD" or "MERGE"
                 PullRequestSCMHead pr = (PullRequestSCMHead) head;
-                if (!(pr.getCheckoutStrategy() == ChangeRequestCheckoutStrategy.MERGE) && pr.getRepository() != null) {
-                    return new BitbucketSCMFileSystem(apiClient, pr.getOriginName(), rev);
+                if (pr.getRepository() == null) { // check access to repository (might be forked)
+                    return null;
                 }
-                return null; // TODO support merge revisions somehow
+
+                if (apiClient instanceof BitbucketCloudApiClient) {
+                    // support lightweight checkout for branches with same owner and repository
+                    if (pr.getCheckoutStrategy() == ChangeRequestCheckoutStrategy.HEAD &&
+                        pr.getRepoOwner().equals(src.getRepoOwner()) &&
+                        pr.getRepository().equals(src.getRepository())) {
+                        ref = pr.getOriginName();
+                    } else {
+                        // Bitbucket cloud does not support refs for pull requests
+                        // Makes lightweight checkout for forks and merge strategy improbable
+                        // TODO waiting for cloud support: https://bitbucket.org/site/master/issues/5814/refify-pull-requests-by-making-them-a-ref
+                        return null;
+                    }
+                } else if (pr.getCheckoutStrategy() == ChangeRequestCheckoutStrategy.HEAD) {
+                    ref = "pull-requests/" + pr.getId() + "/from";
+                } else if (pr.getCheckoutStrategy() == ChangeRequestCheckoutStrategy.MERGE) {
+                    ref = "pull-requests/" + pr.getId() + "/merge";
+                }
             } else if (head instanceof BitbucketTagSCMHead) {
-                 ref = "tags/" + head.getName();
+                ref = "tags/" + head.getName();
             } else {
                 return null;
             }

commit 7c761939e6b950adbadb08ede737ac3b51470002

[JENKINS-56926] Restore support of Jenkinsfile located in subdirectory (#180)
### Your checklist for this pull request

- [x] Make sure you are requesting to **pull a topic/feature/bugfix branch** (right side) and not your master branch!

- [x] Please describe what you did

- [x] Link to relevant GitHub issues or pull requests

- [x] Link to relevant [Jenkins JIRA issues](https://issues.jenkins-ci.org)

- [x] Did you provide a test-case? That demonstrates feature works or fixes the issue.

<!--
Put an `x` into the [ ] to show you have filled the information below
Describe your pull request below
-->

### Description

Addressing regression introduced by https://github.com/brainbeanapps/bitbucket-branch-source-plugin/commit/868676d61ce3847547b6860b928d07c1679c144f in version 2.4.3 (https://issues.jenkins-ci.org/browse/JENKINS-56926)

src/main/java/com/cloudbees/jenkins/plugins/bitbucket/client/BitbucketCloudApiClient.java
@@ -54,6 +54,7 @@ import com.cloudbees.jenkins.plugins.bitbucket.client.repository.UserRoleInRepos
 import com.cloudbees.jenkins.plugins.bitbucket.filesystem.BitbucketSCMFile;
 import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
 import com.damnhandy.uri.template.UriTemplate;
+import com.damnhandy.uri.template.impl.Operator;
 import com.fasterxml.jackson.core.type.TypeReference;
 import edu.umd.cs.findbugs.annotations.CheckForNull;
 import edu.umd.cs.findbugs.annotations.NonNull;
@@ -387,11 +388,11 @@ public class BitbucketCloudApiClient implements BitbucketApi {
     @Override
     public boolean checkPathExists(@NonNull String branchOrHash, @NonNull String path)
             throws IOException, InterruptedException {
-        String url = UriTemplate.fromTemplate(REPO_URL_TEMPLATE + "/src{/branchOrHash,path}")
+        String url = UriTemplate.fromTemplate(REPO_URL_TEMPLATE + "/src{/branchOrHash,path*}")
                 .set("owner", owner)
                 .set("repo", repositoryName)
                 .set("branchOrHash", branchOrHash)
-                .set("path", path)
+                .set("path", path.split(Operator.PATH.getSeparator()))
                 .expand();
         int status = headRequestStatus(url);
         if (HttpStatus.SC_OK == status) {

src/main/java/com/cloudbees/jenkins/plugins/bitbucket/server/client/BitbucketServerAPIClient.java
@@ -147,7 +147,7 @@ public class BitbucketServerAPIClient implements BitbucketApi {
     private static final String API_PULL_REQUESTS_PATH = API_REPOSITORY_PATH + "/pull-requests{?start,limit,at,direction,state}";
     private static final String API_PULL_REQUEST_PATH = API_REPOSITORY_PATH + "/pull-requests/{id}";
     private static final String API_PULL_REQUEST_MERGE_PATH = API_REPOSITORY_PATH + "/pull-requests/{id}/merge";
-    static final String API_BROWSE_PATH = API_REPOSITORY_PATH + "/browse/{+path}{?at}";
+    static final String API_BROWSE_PATH = API_REPOSITORY_PATH + "/browse{/path*}{?at}";
     private static final String API_COMMITS_PATH = API_REPOSITORY_PATH + "/commits{/hash}";
     private static final String API_PROJECT_PATH = API_BASE_PATH + "/projects/{owner}";
     private static final String API_COMMIT_COMMENT_PATH = API_REPOSITORY_PATH + "/commits{/hash}/comments";

src/test/java/com/cloudbees/jenkins/plugins/bitbucket/server/client/BitbucketServerAPIClientTest.java
@@ -1,6 +1,7 @@
 package com.cloudbees.jenkins.plugins.bitbucket.server.client;
 
 import com.damnhandy.uri.template.UriTemplate;
+import com.damnhandy.uri.template.impl.Operator;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -14,7 +15,7 @@ public class BitbucketServerAPIClientTest {
             .fromTemplate(API_BROWSE_PATH)
             .set("owner", "test")
             .set("repo", "test")
-            .set("path", "folder/Jenkinsfile")
+            .set("path", "folder/Jenkinsfile".split(Operator.PATH.getSeparator()))
             .set("at", "fix/test")
             .expand();
         Assert.assertEquals("/rest/api/1.0/projects/test/repos/test/browse/folder/Jenkinsfile?at=fix%2Ftest", expand);
@@ -26,7 +27,7 @@ public class BitbucketServerAPIClientTest {
             .fromTemplate(API_BROWSE_PATH)
             .set("owner", "test")
             .set("repo", "test")
-            .set("path", "Jenkinsfile")
+            .set("path", "Jenkinsfile".split(Operator.PATH.getSeparator()))
             .expand();
         Assert.assertEquals("/rest/api/1.0/projects/test/repos/test/browse/Jenkinsfile", expand);
     }